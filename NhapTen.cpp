52728bec4fc8c22de3780843a8bdf0e36f5e638e
DuyQuang1122 <77633978+DuyQuang1122@users.noreply.github.com>
2021-01-19 01:37:40 +0700

    first commit

---
+++ NhapTen.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib> 
+#include <iostream> 
+  
+using namespace std; 
+  
+int main(int argc, char *argv[]) 
+{ 
+    char name[20]; 
+    cout<<"Nhap ten cua ban:"; 
+    cin>>name; 
+    cout<<"Chao mung "<<name<<" den voi mon hoc LTQL \n"; 
+        
+    system("PAUSE"); 
+    return 0; 
+} 
